40633725419590311f393294163adef7e8059074
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-05-12 15:26:52 +0200

    Add HermesProtocol enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+pub enum HermesProtocol {
+    Hotword,
+    Intent,
+    Speech,
+    SpeechToText,
+    TextToSpeech,
+}
+
+pub trait ToTopic: Sized {
+    fn from_topic(topic: &str) -> Option<Self>;
+    fn as_topic(&self) -> &str;
+}
+
+impl HermesProtocol {
+    fn all() -> Vec<Self> {
+        vec![
+            HermesProtocol::Hotword,
+            HermesProtocol::Intent,
+            HermesProtocol::Speech,
+            HermesProtocol::SpeechToText,
+            HermesProtocol::TextToSpeech,
+        ]
+    }
+}
+
+impl ToTopic for HermesProtocol {
+    fn from_topic(topic: &str) -> Option<Self> {
+        HermesProtocol::all().into_iter().find(|m| m.as_topic() == topic)
+    }
+
+    fn as_topic(&self) -> &str {
+        match *self {
+            HermesProtocol::Hotword => "hermes/hotword",
+            HermesProtocol::Intent => "hermes/intent",
+            HermesProtocol::Speech => "hermes/speech",
+            HermesProtocol::SpeechToText => "hermes/stt",
+            HermesProtocol::TextToSpeech => "hermes/tts",
+        }
     }
 }
