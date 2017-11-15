a6ab61acf35a25e47aff4abe4d8f26cff06980dd
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-11-15 18:21:10 +0100

    better ffi bindings

--- platforms/hermes-kotlin/src/main/kotlin/ai/snips/hermes/ffi/COntology.kt
+++ platforms/hermes-kotlin/src/main/kotlin/ai/snips/hermes/ffi/COntology.kt
@@ -1,23 +1,144 @@
 package ai.snips.hermes.ffi
 
+import ai.snips.hermes.ContinueSessionMessage
+import ai.snips.hermes.EndSessionMessage
 import ai.snips.hermes.IntentMessage
-import ai.snips.hermes.SessionStartedMessage
-import ai.snips.hermes.SessionQueuedMessage
 import ai.snips.hermes.SessionEndedMessage
+import ai.snips.hermes.SessionInit
+import ai.snips.hermes.SessionQueuedMessage
+import ai.snips.hermes.SessionStartedMessage
 import ai.snips.hermes.SessionTermination
+import ai.snips.hermes.SessionTermination.AbortedByUser
+import ai.snips.hermes.SessionTermination.Error
+import ai.snips.hermes.SessionTermination.IntenNotRecognized
 import ai.snips.hermes.SessionTermination.Nominal
 import ai.snips.hermes.SessionTermination.SiteUnAvailable
-import ai.snips.hermes.SessionTermination.AbortedByUser
-import ai.snips.hermes.SessionTermination.IntenNotRecognized
 import ai.snips.hermes.SessionTermination.Timeout
-import ai.snips.hermes.SessionTermination.Error
-
+import ai.snips.hermes.StartSessionMessage
 import ai.snips.queries.ontology.ffi.CIntentClassifierResult
 import ai.snips.queries.ontology.ffi.CSlots
 import ai.snips.queries.ontology.ffi.readString
 import com.sun.jna.Pointer
 import com.sun.jna.Structure
-import com.sun.jna.toJnaPointer
+
+
+class CArrayString(p: Pointer) : Structure(p), Structure.ByReference {
+    init {
+        read()
+    }
+
+    @JvmField
+    var data: Pointer? = null
+    @JvmField
+    var size: Int = -1
+
+    override fun getFieldOrder() = listOf("data", "size")
+
+    fun toStringList() = if (size > 0) {
+        data!!.getPointerArray(0, size).map { it.readString() }
+    } else listOf<String>()
+}
+
+class CActionSessionInit(p: Pointer) : Structure(p), Structure.ByReference {
+    init {
+        read()
+    }
+
+    @JvmField
+    var text: Pointer? = null
+    @JvmField
+    var intent_filter: Pointer? = null
+    @JvmField
+    var can_be_enqueued: Byte = -1
+
+    override fun getFieldOrder() = listOf("text", "intent_filter", "can_be_enqueued")
+
+    fun toSessionInit() = SessionInit.Action(
+            text = text?.readString(),
+            intentFilter = intent_filter?.let { CArrayString(it).toStringList() } ?: listOf(),
+            canBeEnqueued = can_be_enqueued == 1.toByte()
+    )
+}
+
+class CSessionInit : Structure(), Structure.ByValue {
+    companion object {
+        const val ACTION = 1
+        const val NOTIFICATION = 2
+    }
+
+    @JvmField
+    var init_type: Int? = null
+    @JvmField
+    var value: Pointer? = null
+
+    override fun getFieldOrder() = listOf("init_type", "value")
+
+    fun toSessionInit(): SessionInit = when (init_type) {
+        ACTION -> CActionSessionInit(value!!).toSessionInit()
+        NOTIFICATION -> SessionInit.Notification(text = value.readString())
+        else -> throw IllegalArgumentException("unknown value type $init_type")
+    }
+}
+
+class CStartSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
+    init {
+        read()
+    }
+
+    @JvmField
+    var init: CActionSessionInit? = null
+    @JvmField
+    var custom_data: Pointer? = null
+    @JvmField
+    var site_id: Pointer? = null
+
+    override fun getFieldOrder() = listOf("init", "custom_data", "site_id")
+
+    fun toStartSessionMessage() = StartSessionMessage(
+            init = init!!.toSessionInit(),
+            customData = custom_data?.readString(),
+            siteId = site_id?.readString()
+    )
+}
+
+class CContinueSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
+    init {
+        read()
+    }
+
+    @JvmField
+    var session_id: Pointer? = null
+    @JvmField
+    var text: Pointer? = null
+    @JvmField
+    var intent_filter: Pointer? = null
+
+    override fun getFieldOrder() = listOf("session_id", "text", "intent_filter")
+
+    fun toContinueSessionMessage() = ContinueSessionMessage(
+            sessionId = session_id.readString(),
+            text = text.readString(),
+            intentFilter = intent_filter?.let { CArrayString(it).toStringList() } ?: listOf()
+    )
+}
+
+class CEndSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
+    init {
+        read()
+    }
+
+    @JvmField
+    var session_id: Pointer? = null
+    @JvmField
+    var text: Pointer? = null
+
+    override fun getFieldOrder() = listOf("session_id", "text", "intent_filter")
+
+    fun toEndSessionMessage() = EndSessionMessage(
+            sessionId = session_id.readString(),
+            text = text?.readString()
+    )
+}
 
 
 class CIntentMessage(p: Pointer) : Structure(p), Structure.ByReference {
@@ -92,7 +213,7 @@ class CSessionQueuedMessage(p: Pointer) : Structure(p), Structure.ByReference {
             siteId = site_id.readString())
 }
 
-class CSessionTermination: Structure(), Structure.ByValue {
+class CSessionTermination : Structure(), Structure.ByValue {
     companion object {
         const val NOMINAL = 1
         const val SITE_UNAVAILABLE = 2
@@ -102,8 +223,10 @@ class CSessionTermination: Structure(), Structure.ByValue {
         const val ERROR = 6
     }
 
-    @JvmField var termination_type: Int? = null
-    @JvmField var data: Pointer? = null
+    @JvmField
+    var termination_type: Int? = null
+    @JvmField
+    var data: Pointer? = null
 
     override fun getFieldOrder() = listOf("termination_type", "data")
 
