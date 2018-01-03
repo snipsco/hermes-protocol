46f9e0e333ac43c4c4f3674657389c5efa696f34
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2018-01-03 15:05:34 +0100

    add converters to create C hermes objects from kotlin

--- platforms/hermes-kotlin/src/main/kotlin/ai/snips/hermes/ffi/COntology.kt
+++ platforms/hermes-kotlin/src/main/kotlin/ai/snips/hermes/ffi/COntology.kt
@@ -5,6 +5,9 @@ import ai.snips.hermes.EndSessionMessage
 import ai.snips.hermes.IntentMessage
 import ai.snips.hermes.SessionEndedMessage
 import ai.snips.hermes.SessionInit
+import ai.snips.hermes.SessionInit.Action
+import ai.snips.hermes.SessionInit.Notification
+import ai.snips.hermes.SessionInit.Type
 import ai.snips.hermes.SessionQueuedMessage
 import ai.snips.hermes.SessionStartedMessage
 import ai.snips.hermes.SessionTermination
@@ -18,13 +21,26 @@ import ai.snips.hermes.StartSessionMessage
 import ai.snips.queries.ontology.ffi.CIntentClassifierResult
 import ai.snips.queries.ontology.ffi.CSlots
 import ai.snips.queries.ontology.ffi.readString
+import ai.snips.queries.ontology.ffi.toPointer
+import com.sun.jna.Memory
 import com.sun.jna.Pointer
 import com.sun.jna.Structure
 
 
-class CArrayString(p: Pointer) : Structure(p), Structure.ByReference {
+class CArrayString(p: Pointer?) : Structure(p), Structure.ByReference {
     init {
         read()
+    }
+
+    companion object {
+        fun fromStringList(list: List<String>) = CArrayString(null).apply {
+            size = list.size
+            data = Memory(Pointer.SIZE * list.size.toLong()).apply {
+                list.forEachIndexed { i, s ->
+                    this.setPointer(i.toLong() * Pointer.SIZE, s.toPointer())
+                }
+            }
+        }
     }
 
     @JvmField
@@ -39,7 +55,15 @@ class CArrayString(p: Pointer) : Structure(p), Structure.ByReference {
     } else listOf<String>()
 }
 
-class CActionSessionInit(p: Pointer) : Structure(p), Structure.ByReference {
+class CActionSessionInit(p: Pointer?) : Structure(p), Structure.ByReference {
+    companion object {
+        fun fromActionSessionInit(actionSessionInit: SessionInit.Action) = CActionSessionInit(null).apply {
+            text = actionSessionInit.text?.toPointer()
+            intent_filter = if(actionSessionInit.intentFilter.isEmpty()) null else CArrayString.fromStringList(actionSessionInit.intentFilter).pointer
+            can_be_enqueued = if (actionSessionInit.canBeEnqueued) 1 else 0
+        }
+    }
+
     init {
         read()
     }
@@ -64,6 +88,19 @@ class CSessionInit : Structure(), Structure.ByValue {
     companion object {
         const val ACTION = 1
         const val NOTIFICATION = 2
+
+        fun fromSessionInit(sessionInit: SessionInit) = CSessionInit().apply {
+            when(sessionInit.type) {
+                Type.ACTION -> {
+                    init_type = ACTION
+                    value = CActionSessionInit.fromActionSessionInit(sessionInit as Action).pointer
+                }
+                Type.NOTIFICATION -> {
+                    init_type = NOTIFICATION
+                    value = (sessionInit as Notification).text.toPointer()
+                }
+            }
+        }
     }
 
     @JvmField
@@ -80,13 +117,21 @@ class CSessionInit : Structure(), Structure.ByValue {
     }
 }
 
-class CStartSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
+class CStartSessionMessage(p: Pointer?) : Structure(p), Structure.ByReference {
+    companion object {
+        fun fromStartSessionMessage(message: StartSessionMessage) = CStartSessionMessage(null).apply {
+            init = CSessionInit.fromSessionInit(message.init)
+            custom_data = message.customData?.toPointer()
+            site_id = message.siteId?.toPointer()
+        }
+    }
+
     init {
         read()
     }
 
     @JvmField
-    var init: CActionSessionInit? = null
+    var init: CSessionInit? = null
     @JvmField
     var custom_data: Pointer? = null
     @JvmField
@@ -101,9 +146,17 @@ class CStartSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
     )
 }
 
-class CContinueSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
+class CContinueSessionMessage(p: Pointer?) : Structure(p), Structure.ByReference {
     init {
         read()
+    }
+
+    companion object {
+        fun fromContinueSessionMessage(continueSessionMessage: ContinueSessionMessage) = CContinueSessionMessage(null).apply {
+            session_id = continueSessionMessage.sessionId.toPointer()
+            text = continueSessionMessage.text.toPointer()
+            intent_filter =  if(continueSessionMessage.intentFilter.isEmpty()) null else CArrayString.fromStringList(continueSessionMessage.intentFilter).pointer
+        }
     }
 
     @JvmField
@@ -122,7 +175,14 @@ class CContinueSessionMessage(p: Pointer) : Structure(p), Structure.ByReference 
     )
 }
 
-class CEndSessionMessage(p: Pointer) : Structure(p), Structure.ByReference {
+class CEndSessionMessage(p: Pointer?) : Structure(p), Structure.ByReference {
+    companion object {
+        fun fromEndSessionMessage(endSessionMessage: EndSessionMessage) = CEndSessionMessage(null).apply {
+            session_id = endSessionMessage.sessionId.toPointer()
+            text = endSessionMessage.text?.toPointer()
+        }
+    }
+
     init {
         read()
     }
