d81100e3f2fc1dab01e50eda9cb648480f129daf
anthonyray <dantho361@hotmail.com>
2018-03-13 16:22:00 +0100

    adds subscribe callbacks methods

--- hermes_ffi/hermes.py
+++ hermes_ffi/hermes.py
@@ -5,6 +5,7 @@ from ffi.ontology import CProtocolHandler, CDialogueFacade
 from ffi.utils import *
 from time import sleep
 
+
 class Hermes(object):
     def __init__(self, mqtt_server_address):
         self._protocol_handler = POINTER(CProtocolHandler)()  # TODO: Destroy protocol handler
@@ -16,11 +17,35 @@ class Hermes(object):
                                                     byref(self._facade))
 
         # References to user-defined callbacks
-        self._subscribe_intent_callback = callable
+        self._subscribe_intent_callback = None
+        self._subscribe_intents_callback = None
+        self._subscribe_session_started_callback = None
+        self._subscribe_session_queued_callback = None
+        self._subscribe_session_ended_callback = None
 
     def subscribe_intent(self, intent_name, user_callback_subscribe_intent):
         self._subscribe_intent_callback = user_callback_subscribe_intent
         hermes_dialogue_subscribe_intent(self._facade, c_char_p(intent_name), self._subscribe_intent_callback)
+        return self
+
+    def subscribe_intents(self, user_callback_subscribe_intents):
+        self._subscribe_intents_callback = user_callback_subscribe_intents
+        hermes_dialogue_subscribe_intents(self._facade, self._subscribe_intents_callback)
+        return self
+
+    def subscribe_session_started(self, user_callback_subscribe_session_started):
+        self._subscribe_session_started_callback = user_callback_subscribe_session_started
+        hermes_dialogue_subscribe_session_started(self._facade, self._subscribe_session_started_callback)
+        return self
+
+    def subscribe_session_queued(self, user_callback_subscribe_session_queued):
+        self._subscribe_session_queued_callback = user_callback_subscribe_session_queued
+        hermes_dialogue_subscribe_session_started(self._facade, self._subscribe_session_queued_callback)
+        return self
+
+    def subscribe_session_ended(self, user_callback_subscribe_session_ended):
+        self._subscribe_session_ended_callback = user_callback_subscribe_session_ended
+        hermes_dialogue_subscribe_session_started(self._facade, self._subscribe_session_ended_callback)
         return self
 
     def start(self):
