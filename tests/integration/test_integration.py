968137b332d84e81794f007480924df134e94e61
Anthony Reinette <dantho361@hotmail.com>
2018-09-19 12:15:04 +0200

    adds integration tests

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import pytest
+
+
+def test_integration_test():
+    """
+    In this integration test, I would like to spin up a MQTT Broker (such as https://hbmqtt.readthedocs.io/en/latest/). 
+    I would like to publish messages to the MQTT broker and check that the hermes client parses the messages correctly. 
+    """
+    assert True == True
+
