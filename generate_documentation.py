1234f2695efdf138ddedec40970a454506687263
Anthony Reinette <dantho361@hotmail.com>
2018-10-04 16:12:54 +0200

    updating script to generates documentation

--- generate_documentation.py
+++ generate_documentation.py
@@ -7,6 +7,14 @@ import inspect
 import pydoc
 import sys
 
+""" Configuration section """
+
+METHODS_TO_IGNORE = ["__unicode__", "__nonzero__", "__native__", "__long__", "next", "_DotMap__call_items", "_OrderedDict__update"]
+CLASSES_TO_IGNORE = []
+
+""" End of configuration section """
+
+
 package_depth = 3
 package_header_template = "{} {}".format(package_depth * '#','{}')
 
@@ -35,7 +43,7 @@ def import_module_safely(module_name):
 
 def generate_docs():
     package_names = ["hermes_python"]
-    packages = [import_module_safely(pack_name) for pack_name in package_names]
+    packages = [import_module_safely(package_name) for package_name in package_names]
     return getmarkdown(packages[0])
 
 
@@ -49,13 +57,19 @@ def getmarkdown(package):
         output.append(module_header_template.format(mod_name))
 
         classes = pydoc.inspect.getmembers(mod, pydoc.inspect.isclass)
+        filtered_classes = filter(lambda (cls_name, cls): cls_name not in CLASSES_TO_IGNORE, classes)
 
-        for cls_name, cls in classes:
+        for cls_name, cls in filtered_classes:
             if cls.__module__ == (package.__name__ + "." + mod_name):
                 output.append(class_header_template.format(mod_name, cls_name))  # We only take built-in classes. Not the ones that were brought into scope.
-                for mth_name, mth in pydoc.inspect.getmembers(cls, pydoc.inspect.ismethod):
-                    output.append(method_header_template.format(mod_name+"."+cls_name+"."+mth_name))
-                    output.append("*{}*".format(pydoc.inspect.formatargspec(*pydoc.inspect.getargspec(mth))))  # Adds signature
+
+                methods = pydoc.inspect.getmembers(cls, pydoc.inspect.ismethod)
+                filtered_methods = filter(lambda (method_name, method): method_name not in METHODS_TO_IGNORE, methods)
+
+                for mth_name, mth in filtered_methods:
+                    output.append(method_header_template.format(mod_name + "." + cls_name + "." + mth_name))
+                    output.append(
+                        "*{}*".format(pydoc.inspect.formatargspec(*pydoc.inspect.getargspec(mth))))  # Adds signature
                     if pydoc.inspect.getdoc(mth):
                         output.append("```")
                         output.append(pydoc.inspect.getdoc(mth))
