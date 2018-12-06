93f594b6c4acaf1fc7e75aaeb8ead5f2009c0bf0
anthonyray <anthony.reinette@snips.ai>
2018-12-06 16:35:31 +0100

    fix test script in container

--- platforms/hermes-python/build-wheels.sh
+++ platforms/hermes-python/build-wheels.sh
@@ -23,4 +23,4 @@ PYBIN=/opt/python/cp27-cp27m/bin
 fixed_whl=hermes_python-0.1.24-cp27-cp27m-manylinux1_x86_64.whl
 # Test
 ${PYBIN}/pip install ${fixed_whl}
-${PYBIN}/python -c "import hermes_python.hermes.Hermes"
+${PYBIN}/python -c "from hermes_python.hermes import Hermes"
