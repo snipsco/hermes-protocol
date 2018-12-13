4f10d4ef213d41547283e67c21e9a89ace2d6b80
anthonyray <anthony.reinette@snips.ai>
2018-12-13 14:50:47 +0100

    updating macos build scripts

--- platforms/hermes-python/build_scripts/build_macos_wheels.sh
+++ platforms/hermes-python/build_scripts/build_macos_wheels.sh
@@ -2,21 +2,30 @@
 set -e -x
 
 # Build the .so file
-if ! [[ -z "$(ls -A hermes_python/dylib)" ]]; then
+if ! [[ -z "$(ls -A ../hermes_python/dylib)" ]]; then
    echo "hermes_python/dylib should be empty. Aborting!" && exit 1
 fi
 
-CARGO_TARGET_DIR=./target cargo rustc --lib --manifest-path ../../hermes-mqtt-ffi/Cargo.toml --release -- --crate-type cdylib || exit 1
+CARGO_TARGET_DIR=../target cargo rustc --lib --manifest-path ../../../hermes-mqtt-ffi/Cargo.toml --release -- --crate-type cdylib || exit 1
 
-mv target/release/libhermes_mqtt_ffi.dylib hermes_python/dylib/
+mv ../target/release/libhermes_mqtt_ffi.dylib ../hermes_python/dylib/
 
 # Build wheel
-virtualenv env
-source env/bin/activate
+cd ..
+# Build wheel
+PYTHON_INTERPRETERS=( 'python2.7' 'python3.6' 'python3.7')
 
-python setup.py bdist_wheel
+for PYINTERPRETER in "${PYTHON_INTERPRETERS[@]}";
+do
+	echo $PYINTERPRETER
+	virtualenv --python=$PYINTERPRETER env
+	source env/bin/activate
+	python setup.py bdist_wheel 
+	rm -rf env
+done
 
 # Clean up after yourself
 rm hermes_python/dylib/*.dylib
 
 ls -1 dist/
+cd build_scripts
