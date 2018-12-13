3cae24ed58939aab0ce3190ed82adf135690fc8a
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2018-12-13 14:17:59 +0100

    update test.c to use the new mqtt config

--- platforms/c/test.c
+++ platforms/c/test.c
@@ -8,16 +8,48 @@ void callback(const CSayMessage *ptr, void* user_data) {
 	printf("user data is %s\n", user_data);
 }
 
+const char * last_error() {
+    const char *nullerror = NULL;
+    const char **error = & nullerror;
+
+    hermes_get_last_error(error);
+
+    return * error; // we leak the string here but well crash just after so that's not that big a problem
+
+}
+
+#define check(hermes_expr) if( hermes_expr != SNIPS_RESULT_OK ) { printf("Assertion failed at %s:%i in function %s:\nFailed to execute %s\nError was %s\n", __FILE__, __LINE__, __func__, #hermes_expr, last_error()  ); exit(1); }
+
 int main() {
-	hermes_enable_debug_logs();
+	check(hermes_enable_debug_logs());
 	const CProtocolHandler *truc;
 	printf("new\n");
-	hermes_protocol_handler_new_mqtt(&truc, "localhost:1883", "my user data");
+
+    const char *value_array[] = {"/path/to/cafile"};
+    CStringArray ca_files = {.data = value_array,
+                                   .size = sizeof(value_array) /
+                                           sizeof(value_array[0])};
+
+	const CMqttOptions options = {
+	    .broker_address = "localhost:1883",
+	    .username = NULL,
+	    .password = NULL,
+	    .tls_hostname = NULL,
+	    .tls_ca_file = &ca_files,
+	    .tls_ca_path = NULL,
+	    .tls_client_key = NULL,
+	    .tls_client_cert = NULL,
+	    .tls_disable_root_store = 0
+	};
+
+    check(hermes_protocol_handler_new_mqtt_with_options(&truc, &options, "my user data"));
+
+
 	printf("new done\n");
 	const CTtsBackendFacade *tts;
-	hermes_protocol_handler_tts_backend_facade(truc, &tts);
+	check(hermes_protocol_handler_tts_backend_facade(truc, &tts));
 	printf("pointer in C : %p\n", callback);
-	hermes_tts_backend_subscribe_say(tts, callback);
+	check(hermes_tts_backend_subscribe_say(tts, callback));
 
 	while (true)  {}
 
