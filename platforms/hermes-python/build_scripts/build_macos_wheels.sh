398978a7ee1d32059b9a2ee0c518d2faae52c6c3
anthonyray <anthony.reinette@snips.ai>
2019-01-02 15:18:18 +0100

    harmonizing execution directory for wheel building scripts (1/2)

--- platforms/hermes-python/build_scripts/build_macos_wheels.sh
+++ platforms/hermes-python/build_scripts/build_macos_wheels.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
+# This script should be ran from the root of the repository
 set -e -x
 
+
 # Build the .so file
-if ! [[ -z "$(ls -A ../hermes_python/dylib)" ]]; then
+if ! [[ -z "$(ls -A platforms/hermes-python/hermes_python/dylib)" ]]; then
    echo "hermes_python/dylib should be empty. Aborting!" && exit 1
 fi
 
-CARGO_TARGET_DIR=../target cargo rustc --lib --manifest-path ../../../hermes-mqtt-ffi/Cargo.toml --release -- --crate-type cdylib || exit 1
+CARGO_TARGET_DIR=platforms/hermes-python/target cargo rustc --lib --manifest-path hermes-mqtt-ffi/Cargo.toml --release -- --crate-type cdylib || exit 1
 
-mv ../target/release/libhermes_mqtt_ffi.dylib ../hermes_python/dylib/
+mv platforms/hermes-python/target/release/libhermes_mqtt_ffi.dylib platforms/hermes-python/hermes_python/dylib/
 
 # Build wheel
-cd ..
+cd platforms/hermes-python/
 # Build wheel
 PYTHON_INTERPRETERS=( 'python2.7' 'python3.6' 'python3.7')
 
@@ -28,6 +30,7 @@ done
 rm hermes_python/dylib/*.dylib
 
 ls -1 dist/
-cp dist/ wheelhouse/
+mkdir -p wheelhouse
+cp dist/*.whl wheelhouse/
 rm -rf dist/*.whl
-cd build_scripts
+cd ../..
