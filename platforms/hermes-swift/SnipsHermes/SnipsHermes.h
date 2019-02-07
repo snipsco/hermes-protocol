1e1bf1c8edef3621958cde1963f91e2b41c19b3f
Kevin Lefevre <kevin.lefevre@snips.ai>
2019-02-07 17:11:15 +0100

    Bootstrap xcode project

---
+++ platforms/hermes-swift/SnipsHermes/SnipsHermes.h
@@ -0,0 +1,19 @@
+//
+//  SnipsHermes.h
+//  SnipsHermes
+//
+//  Created by Kevin Lefevre on 07/02/2019.
+//  Copyright © 2019 Snips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnipsHermes.
+FOUNDATION_EXPORT double SnipsHermesVersionNumber;
+
+//! Project version string for SnipsHermes.
+FOUNDATION_EXPORT const unsigned char SnipsHermesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnipsHermes/PublicHeader.h>
+
+
