3c9e9ecc2b96ca456b181551d3bece76f0ad129e
anthonyray <dantho361@hotmail.com>
2019-03-21 13:51:58 +0100

    adding mechanism to load mock FFI library

--- platforms/hermes-python/hermes_python/ffi/__init__.py
+++ platforms/hermes-python/hermes_python/ffi/__init__.py
@@ -5,12 +5,21 @@ import os
 from glob import glob
 from ctypes import cdll
 
-DYLIB_NAME = "libhermes_mqtt_ffi.*"
-DYLIB_DIR = os.path.join(os.path.dirname(__file__), "../dylib")
-DYLIB_PATH = glob(os.path.join(DYLIB_DIR, DYLIB_NAME))[0]
 
-lib = cdll.LoadLibrary(DYLIB_PATH)
+class MockedLib(object):  # This class is here in case you need to mock calls to the actual library.
+    def __getattr__(self, item):
+        return MockedLib()
+
+    def __call__(self, *args, **kwargs):
+        raise LibException("Trying to call mocked FFI library")
+
 
 class LibException(Exception):
     pass
 
+
+DYLIB_NAME = "libhermes_mqtt_ffi.*"
+DYLIB_DIR = os.path.join(os.path.dirname(__file__), "../dylib")
+DYLIB_PATHS = glob(os.path.join(DYLIB_DIR, DYLIB_NAME))
+
+lib = cdll.LoadLibrary(DYLIB_PATHS[0]) if len(DYLIB_PATHS) > 0 else MockedLib()
